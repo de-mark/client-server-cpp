198b4310fe73cd12b09cddb18a5373d7734a0301
de-mark <data.engineer.mark@gmail.com>
2023-04-01 20:29:29 -0700

    Added basic port / ip parsing to server and client

--- client.cpp
+++ client.cpp
@@ -4,6 +4,15 @@
 
 // Takes in client ip and port parameters when initially run
 int main(int argc, char *argv[]){
+    // We need ip address AND port for the client
+    if (argc != 3) {
+        std::cerr << "Please provide (1) ip address and (2) port to run chat client." << std::endl;
+        exit(0);
+    }
+
+    const char *SERVER_IP = argv[1]; 
+    const int PORT = std::atoi(argv[2]);
+    char message[3000];
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -4,6 +4,16 @@
 
 // Takes in server port parameters when initially run
 int main(int argc, char *argv[]){
+    
+    // We at least need to know what port the server will run on
+    // Closes program if this is not provided
+    if (argc != 2) {
+        std::cerr << "Please provide a port to run the server on." << std::endl;
+        exit(0);
+    }
+
+    const int PORT = std::atoi(argv[1]);
+    char message[3000];
 
     return 0;
 }
